a0f2a2889d6c07e9a150d962c1eaf9c6c301c32b
Anton Lykhoyda <lykhoyda@gmail.com>
2023-03-14 17:51:58 +0100

    add side bar styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
-import { Outlet } from 'react-router-dom';
-import { SideMenu } from './components/SideMenu';
-import { WalletContextProvider } from './context/WalletContext';
-import { StorageContextProvider } from './context/StorageContext';
-import { FlowContextProvider } from './context/FlowContext';
+import {Outlet} from 'react-router-dom';
+import styled from 'styled-components';
+import {SideMenu} from './components/SideMenu';
+import {WalletContextProvider} from './context/WalletContext';
+import {StorageContextProvider} from './context/StorageContext';
+import {FlowContextProvider} from './context/FlowContext';
 
 export function App(): JSX.Element {
-  return (
-    <WalletContextProvider>
-      <StorageContextProvider>
-        <FlowContextProvider>
-          <>
-            <Outlet />
-            <SideMenu />
-          </>
-        </FlowContextProvider>
-      </StorageContextProvider>
-    </WalletContextProvider>
-  );
+    return (
+        <WalletContextProvider>
+            <StorageContextProvider>
+                <FlowContextProvider>
+                    <Layout>
+                        <Outlet/>
+                        <SideMenu/>
+                    </Layout>
+                </FlowContextProvider>
+            </StorageContextProvider>
+        </WalletContextProvider>
+    );
 }
+
+const Layout = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--s1);
+  height: 100vh;
+
+  & > :first-child {
+    flex-grow: 1;
+    max-width: 212px;
+    width: 100%;
+  }
+
+  & > :last-child {
+    flex-basis: 0;
+    flex-grow: 99999;
+    min-inline-size: 50%;
+  }
+`;
--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,18 +1,70 @@
-import './SideMenu.scss';
-import { Spinner } from '../Spinner';
-import { Checkmark, Failure } from '../icons';
-import { Heading } from '../Heading';
+import styled from 'styled-components';
+import projectIcon from '../../assets/icons/project-icon.svg';
+import chestIcon from '../../assets/icons/chest-icon.svg';
+import heartIcon from '../../assets/icons/heart-icon.svg';
 
 export function SideMenu(): JSX.Element {
   return (
-    <div className="sidebar">
-      <menu>
-        <Heading>Logo</Heading>
-        <Spinner />
-        <Checkmark />
-        <Failure />
-        <Heading>Logo</Heading>
-      </menu>
-    </div>
+    <SidebarWrapper>
+      <ProjectIconWrapper href={'#'}>
+        <ProjectIcon src={projectIcon} alt="project icon" />
+        <ProjectName>Testnet Validator Launchpad</ProjectName>
+      </ProjectIconWrapper>
+      <nav>
+        <MenuItem href={'#'}>
+          <img src={chestIcon} alt="chest icon" />
+          Home
+        </MenuItem>
+        <MenuItem href={'#'}>
+          <img src={heartIcon} alt="FAQ" />
+          FAQ
+        </MenuItem>
+      </nav>
+    </SidebarWrapper>
   );
 }
+
+const SidebarWrapper = styled.div`
+  padding: 13px 0 0 15px;
+  background: #0f101b;
+`;
+
+const ProjectIconWrapper = styled.a`
+  cursor: pointer;
+  display: flex;
+  flex-wrap: nowrap;
+  margin-bottom: 31px;
+`;
+
+const ProjectIcon = styled.img`
+  max-width: 59px;
+  max-height: 59px;
+  width: 100%;
+`;
+
+const ProjectName = styled.div`
+  color: white;
+  align-self: center;
+  word-spacing: 100vw;
+  margin-left: 2px;
+  font-weight: 500;
+  font-size: 0.75rem;
+  line-height: 0.8125rem;
+`;
+
+const MenuItem = styled.a`
+  cursor: pointer;
+  padding: 8px 16px;
+  max-width: 120px;
+  color: #959eb9;
+  display: flex;
+  background: #141925;
+  border-radius: 12px;
+  margin-bottom: 8px;
+
+  img {
+    max-width: 22px;
+    width: 100%;
+    margin-right: 11px;
+  }
+`;
