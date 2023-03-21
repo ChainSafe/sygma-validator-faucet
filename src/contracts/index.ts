89d38e32bdd1c30c6aa260ea99186d73c4d75921
Bernard <bero4net@gmail.com>
2023-03-21 14:04:06 +0100

    refactor contracts

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -1,2 +1,6 @@
-export * from './abi';
+// ABI's
+import { DepositAdapterABI } from './depositAdapter.abi';
+import { BasicFeeHandlerABI } from './basicFeeHandler.abi';
+
 export * from './addresses';
+export { DepositAdapterABI, BasicFeeHandlerABI };
