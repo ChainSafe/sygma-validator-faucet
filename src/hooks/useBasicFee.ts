1617ac9a9b0f55b2a9ff54fa10c8bb37fc4db615
Bernard <bero4net@gmail.com>
2023-03-21 14:05:46 +0100

    collecting fee info from a chain

---
+++ src/hooks/useBasicFee.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import { useEnsuredWallet } from '../context/WalletContext';
+import {
+  BasicFeeHandlerABI,
+  MOONBASE_BASIC_FEE_ADDRESS,
+  MUMBAI_BASIC_FEE_ADDRESS,
+} from '../contracts';
+
+const BRIDGE_FEE = '0.001';
+const bridgeFeeWei = Web3.utils.toWei(BRIDGE_FEE, 'ether');
+const defaultFee = Web3.utils.toBigInt(bridgeFeeWei);
+
+const getAddressByChainID = (chainID: string): string | false => {
+  switch (chainID) {
+    case '0x507':
+      return MOONBASE_BASIC_FEE_ADDRESS;
+    case '0x13881':
+      return MUMBAI_BASIC_FEE_ADDRESS;
+    default:
+      return false;
+  }
+};
+
+export function useBasicFee(): [bigint] {
+  const wallet = useEnsuredWallet();
+
+  const [fee, setFee] = useState(defaultFee);
+
+  useEffect(() => {
+    const address = getAddressByChainID(wallet.chainId);
+    if (!address) return;
+
+    new wallet.web3.eth.Contract(BasicFeeHandlerABI, address).methods
+      ._fee()
+      .call()
+      .then((result) => {
+        if (typeof result !== 'bigint') return;
+        setFee(result);
+      })
+      .catch((error) => {
+        console.error('useBasicFee error', error);
+        setFee(defaultFee);
+      });
+  }, [wallet.web3, wallet.chainId]);
+
+  return [fee];
+}
